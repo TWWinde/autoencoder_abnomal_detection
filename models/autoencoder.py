8e3cb9259a36d640728a15150f0fa48cadb2bd75
wenwutang <twwinde@gmail.com>
2023-08-05 13:08:02 +0200

    model

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -1,19 +1,19 @@
-import keras
+#import keras
 import numpy as np
 import tensorflow as tf
-from keras import Model
+from tensorflow.keras import Model
 
 
 class Autoencoder(Model):
-    def __init__(self, kernel_size=(3, 3)):
+    def __init__(self):
         super(Autoencoder, self).__init__()
 
         self.encoder = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(16, kernel_size, padding='same', activation=tf.nn.relu, input_shape=(96, 96, 3)),
+            tf.keras.layers.Conv2D(16, (3,3), padding='same', activation=tf.nn.relu, input_shape=(96, 96, 3)),
             tf.keras.layers.MaxPooling2D(pool_size=(4, 4), padding='same'),
-            tf.keras.layers.Conv2D(8, kernel_size, activation='relu', padding='same'),
+            tf.keras.layers.Conv2D(8, (3,3), activation='relu', padding='same'),
             tf.keras.layers.MaxPooling2D(pool_size=(4, 4), padding='same'),
-            tf.keras.layers.Conv2D(3, kernel_size, activation='relu', padding='same'),
+            tf.keras.layers.Conv2D(3, (3,3), activation='relu', padding='same'),
             tf.keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same'),
 
         ])
@@ -35,11 +35,13 @@ class Autoencoder(Model):
         return decoded
 
 
-random_float_array = np.random.random((96, 96, 3))
-print(random_float_array)
+x = np.random.random((1, 96, 96, 3))
+autoencoder= Autoencoder()
 
 
-def encoder(inputs, kernel_size):
+
+
+def encoder(inputs, kernel_size = (3, 3)):
     out = tf.keras.layers.Conv2D(16, kernel_size, padding='same', activation=tf.nn.relu, input_shape=(96, 96, 3))(
         inputs)
     out = tf.keras.layers.MaxPooling2D(pool_size=(4, 4), padding='same')(out)
@@ -51,7 +53,7 @@ def encoder(inputs, kernel_size):
     return out
 
 
-def decoder(inputs, filters, kernel_size):
+def decoder(inputs):
     out = tf.keras.layers.Conv2D(16, (3, 3), padding='same', activation=tf.nn.relu, input_shape=(96, 96, 3))(inputs)
     out = tf.keras.layers.MaxPooling2D(pool_size=(4, 4), padding='same')(out)
     out = tf.keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(out)
@@ -60,3 +62,7 @@ def decoder(inputs, filters, kernel_size):
     out = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same')(out)
 
     return out
+
+latent = encoder(x)
+y = decoder(latent)
+print(y.shape)
